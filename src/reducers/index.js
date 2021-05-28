731aa73b7dd3edfc2e69bb06aec19a8a9cc88a91
Eugene <clackd1amond@users.noreply.github.com>
2021-05-28 20:31:26 +0300

    loading spinner and error

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,12 +1,28 @@
 const initialState = {
 	menu: [],
+	loading: true,
 };
 
 const reducer = (state = initialState, action) => {
+	console.log(state);
 	switch (action.type) {
 		case 'MENU_LOADED':
 			return {
 				menu: action.payload,
+				loading: false,
+				error: false,
+			};
+		case 'MENU_REQUESTED':
+			return {
+				menu: state.menu,
+				loading: true,
+				error: false,
+			};
+		case 'MENU_ERROR':
+			return {
+				menu: state.menu,
+				loading: true,
+				error: true,
 			};
 		default:
 			return state;
