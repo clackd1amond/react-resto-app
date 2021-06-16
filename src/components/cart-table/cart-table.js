655dea1aae73969377863fa24f4912a42b2543ff
Eugene <clackd1amond@users.noreply.github.com>
2021-06-16 15:29:54 +0300

    total price, send order

--- src/components/cart-table/cart-table.js
+++ src/components/cart-table/cart-table.js
@@ -2,21 +2,24 @@ import React from 'react';
 import './cart-table.scss';
 import { connect } from 'react-redux';
 import { deleteFromCart } from '../../actions';
+import WithRestoService from '../hoc';
 
-const CartTable = ({ items, deleteFromCart }) => {
-	if (items.length === 0) return <div className='cart__title'> Ваша корзина пуста :( </div>;
+const CartTable = ({ items, deleteFromCart, RestoService }) => {
+	if (items.length === 0) return <div className='cart__title'>Ваша корзина пуста</div>;
 
 	return (
 		<>
 			<div className='cart__title'>Ваш заказ:</div>
 			<div className='cart__list'>
 				{items.map((item) => {
-					const { title, price, url, id } = item;
+					const { title, price, url, id, qtty } = item;
 					return (
 						<div key={id} className='cart__item'>
 							<img src={url} className='cart__item-img' alt={title} />
 							<div className='cart__item-title'>{title}</div>
-							<div className='cart__item-price'>${price}</div>
+							<div className='cart__item-price'>
+								${price} x{qtty}
+							</div>
 							<div onClick={() => deleteFromCart(id)} className='cart__close'>
 								&times;
 							</div>
@@ -24,13 +27,29 @@ const CartTable = ({ items, deleteFromCart }) => {
 					);
 				})}
 			</div>
+			<button
+				onClick={() => {
+					RestoService.setOrder(generateOrder(items));
+				}}
+				className='order'
+			>
+				Оформить заказ
+			</button>
 		</>
 	);
 };
 
-const mapStateToProps = ({ items }) => ({ items });
-const mapDispatchToProps = {
-	deleteFromCart,
+const generateOrder = (items) => {
+	const newOrder = items.map((item) => {
+		return {
+			id: item.id,
+			qtty: item.qtty,
+		};
+	});
+	return newOrder;
 };
 
-export default connect(mapStateToProps, mapDispatchToProps)(CartTable);
+const mapStateToProps = ({ items }) => ({ items });
+const mapDispatchToProps = { deleteFromCart };
+
+export default WithRestoService()(connect(mapStateToProps, mapDispatchToProps)(CartTable));
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,6 +3,7 @@ const initialState = {
 	loading: true,
 	error: false,
 	items: [],
+	totalPrice: 0,
 };
 
 const reducer = (state = initialState, action) => {
@@ -26,23 +27,42 @@ const reducer = (state = initialState, action) => {
 			};
 		case 'ITEM_ADD_TO_CART':
 			const id = action.payload;
+
+			const itemInd = state.items.findIndex((item) => item.id === id);
+			if (itemInd >= 0) {
+				const itemInState = state.items.find((item) => item.id === id);
+				const newItem = {
+					...itemInState,
+					qtty: ++itemInState.qtty,
+				};
+				return {
+					...state,
+					items: [...state.items.slice(0, itemInd), newItem, ...state.items.slice(itemInd + 1)],
+					totalPrice: state.totalPrice + newItem.price,
+				};
+			}
+
 			const item = state.menu.find((item) => item.id === id);
 			const newItem = {
 				title: item.title,
 				price: item.price,
 				url: item.url,
 				id: item.id,
+				qtty: 1,
 			};
 			return {
 				...state,
 				items: [...state.items, newItem],
+				totalPrice: state.totalPrice + newItem.price,
 			};
 		case 'ITEM_REMOVE_FROM_CART':
 			const idx = action.payload;
 			const itemIndex = state.items.findIndex((item) => item.id === idx);
+			const price = state.items[itemIndex].price * state.items[itemIndex].qtty;
 			return {
 				...state,
 				items: [...state.items.slice(0, itemIndex), ...state.items.slice(itemIndex + 1)],
+				totalPrice: state.totalPrice - price,
 			};
 		default:
 			return state;
