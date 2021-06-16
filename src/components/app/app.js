11f7ade4d55f60415657c6809bb7f8c63bb78aa2
Eugene <clackd1amond@users.noreply.github.com>
2021-06-16 13:45:12 +0300

    item page

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { MainPage, CartPage } from '../pages';
+import { MainPage, CartPage, ItemPage } from '../pages';
 import AppHeader from '../app-header';
 
 import Background from './food-bg.jpg';
@@ -12,6 +12,7 @@ const App = () => {
 			<Switch>
 				<Route path='/' component={MainPage} exact />
 				<Route path='/cart' component={CartPage} />
+				<Route path='/:id' component={ItemPage} />
 			</Switch>
 		</div>
 	);
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,27 +1,28 @@
 const initialState = {
 	menu: [],
 	loading: true,
+	error: false,
 };
 
 const reducer = (state = initialState, action) => {
 	console.log(state);
 	switch (action.type) {
-		case 'MENU_LOADED':
-			return {
-				menu: action.payload,
-				loading: false,
-				error: false,
-			};
 		case 'MENU_REQUESTED':
 			return {
 				menu: state.menu,
 				loading: true,
 				error: false,
 			};
+		case 'MENU_LOADED':
+			return {
+				menu: action.payload,
+				loading: false,
+				error: false,
+			};
 		case 'MENU_ERROR':
 			return {
 				menu: state.menu,
-				loading: true,
+				loading: false,
 				error: true,
 			};
 		default:
